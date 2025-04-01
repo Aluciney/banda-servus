2392f42b4c1843e020951e9d8bd0729249a26a2d
Aluciney de Souza Wanderley <aluciney.wanderley@amazonasenergia.com>
2025-04-01 13:05:00 -0400

    Criando Estrutura

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,32 @@
 import React from 'react';
 import { Header } from './components/Header';
 import { Footer } from './components/Footer';
+import GrupoImage from './assets/grupo.jpg';
 
 
 export const App: React.FC = () => {
   return (
-    <div className="relative max-h-lvh">
+    <div className="min-h-lvh bg-black">
       <Header />
-      <main className="pt-16">
-        <section id="home" className="h-screen bg-gray-200">
-          <h1 className="text-5xl text-center font-bold text-gray-800">Welcome to Banda Servus</h1>
-          <p className="text-center text-gray-600">A digital platform for musicians, bands, and fans</p>
+      <main className="pt-20 max-w-screen-xl mx-auto">
+        <section id="home" className="h-screen bg-gray-200 scroll-mt-20 py-16" style={{ backgroundImage: 'url(/grupo.jpg)', backgroundSize: 'cover', backgroundPosition: 'center' }}>
+          <h1 className="text-5xl sm:text-7xl md:text-8xl lg:text-9xl text-center font-bold text-white">Banda Servus</h1>
+          <div className="">
+            <div className="flex flex-col items-center sm:items-end text-gray-400 px-4">
+              <p>"Ide por tudo mundo, proclamai o Evangelho a toda criatura"</p>
+              <p>Marcos 16:15</p>
+            </div>
+          </div>
         </section>
-        <section id="photos" className="h-screen bg-gray-500">
+        <section id="photos" className="h-screen bg-gray-500 scroll-mt-20">
           <h2 className="text-4xl text-center font-bold text-gray-800">Photos</h2>
           <p className="text-center text-gray-600">Check out our latest photos from gigs and events.</p>
         </section>
-        <section id="about" className="h-screen bg-gray-300">
+        <section id="about" className="h-screen bg-gray-300 scroll-mt-20">
           <h2 className="text-4xl text-center font-bold text-gray-800">About Us</h2>
           <p className="text-center text-gray-600">We are a band that loves to play music and connect with our fans.</p>
         </section>
-        <section id="contact" className="h-screen bg-gray-400">
+        <section id="contact" className="h-screen bg-gray-400 scroll-mt-20">
           <h2 className="text-4xl text-center font-bold text-gray-800">Contact Us</h2>
           <p className="text-center text-gray-600">Feel free to reach out to us for any inquiries or collaborations.</p>
         </section>
--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,15 +1,17 @@
 export const Header: React.FC = () => {
 	return (
-		<header className="fixed flex justify-between items-center p-4 bg-gray-800 text-white">
-			<h1>Portfólio</h1>
-			<nav>
-				<ul className="flex space-x-4">
-					<li><a href="#home">Home</a></li>
-					<li><a href="#photos">Fotos</a></li>
-					<li><a href="#about">Sobre</a></li>
-					<li><a href="#contact">Contato</a></li>
-				</ul>
-			</nav>
+		<header className="fixed px-12 h-20 bg-zinc-800 text-white w-full">
+			<div className="flex justify-between items-center h-full max-w-screen-xl mx-auto text-sm">
+				<h1 className="hidden sm:block">Portfólio</h1>
+				<nav className="w-full">
+					<ul className="flex w-full space-x-12 justify-center sm:justify-end">
+						<li><a href="#home" className="hover:underline">Home</a></li>
+						<li><a href="#photos" className="hover:underline">Fotos</a></li>
+						<li><a href="#about" className="hover:underline">Sobre</a></li>
+						<li><a href="#contact" className="hover:underline">Contato</a></li>
+					</ul>
+				</nav>
+			</div>
 		</header>
 	);
 }
